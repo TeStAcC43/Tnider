d8013bb8fd197f751b3216b58862739eab9f411b
Aleksandras <aleksxx98@gmail.com>
2020-05-07 18:27:30 +0300

    basics

---
+++ index.js
@@ -0,0 +1,38 @@
+function Submit(e) {
+  event.preventDefault();
+
+  console.log($('#Tnider').serializeArray());
+  Email.send({
+    SecureToken: "c3ef64d7-dfb7-4b87-9db7-d8ab6474a6b3",
+    To: 'testaleksandras@gmail.com',
+    From: "testaleksandras@gmail.com",
+    Subject: "Tnider",
+    Body: $('#Tnider').serializeArray()
+  })
+}
+
+window.addEventListener("click", () => {
+  sound = document.querySelector('audio');
+  sound.play();
+});
+
+$(function () {
+  const $select = $(".year");
+  const date = new Date();
+  for (i = 1; i <= date.getFullYear(); i++) {
+    $select.append($('<option></option>').val(i).html(i))
+  }
+  const $select2 = $(".month");
+  for (i = 1; i <= 12; i++) {
+    $select2.append($('<option></option>').val(i).html(i))
+  }
+  const $select3 = $(".day");
+  for (i = 1; i <= 31; i++) {
+    $select3.append($('<option></option>').val(i).html(i))
+  }
+});
+
+
+
+
+
